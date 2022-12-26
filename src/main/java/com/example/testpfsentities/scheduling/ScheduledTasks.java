6c4f34b44d67d945eddd370e134b8b0a3b083f17
Fatihabardiche <bardichefatiha@gmail.com>
2022-12-26 22:10:18 +0100

    add change match status to pending and played

--- src/main/java/com/example/testpfsentities/scheduling/ScheduledTasks.java
+++ src/main/java/com/example/testpfsentities/scheduling/ScheduledTasks.java
@@ -1,11 +1,13 @@
 package com.example.testpfsentities.scheduling;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import com.example.testpfsentities.entities.Match;
 import com.example.testpfsentities.entities.Player;
+import com.example.testpfsentities.entities.enums.MatchStatus;
 import com.example.testpfsentities.repository.MatchRepository;
 import com.example.testpfsentities.service.MatchService;
 import com.example.testpfsentities.service.PlayerService;
@@ -34,22 +36,66 @@ public class ScheduledTasks {
 
     @Scheduled(cron = "* * * * * *")
     public void evaluateMatchAfterOneHour() {
-        Date newDate = new Date(System.currentTimeMillis() - 3600 * 1000);
-
+        Date currentDate = new Date(System.currentTimeMillis() - 3600 * 1500);
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(currentDate);
+        int currentYear = cal1.get(Calendar.YEAR);
+        int currentMonth = cal1.get(Calendar.MONTH);
+        int currentDay = cal1.get(Calendar.DAY_OF_MONTH);
         //get the list of match that have the date dateTime
         try {
             List<Match> listMatches = matchRepository.findAll();
             for (Match match : listMatches) {
-                if (match.getDate().getHours() == newDate.getHours()
-                        && match.getDate().getMinutes() == newDate.getMinutes()
-                        && match.getDate().getSeconds() == newDate.getSeconds()
-                ) {
-                    Player ownerMatchPlayer = playerService.returnOwnerMatchPlayer(match);
-                    matchService.sendNotificationOfMatchEvaluation(match, ownerMatchPlayer);
+                Calendar cal2 = Calendar.getInstance();
+                cal2.setTime(match.getDate());
+                int otherYear = cal2.get(Calendar.YEAR);
+                int otherMonth = cal2.get(Calendar.MONTH);
+                int otherDay = cal2.get(Calendar.DAY_OF_MONTH);
+                if (currentYear == otherYear && currentMonth == otherMonth && currentDay == otherDay){
+                    if (match.getDate().getHours() == currentDate.getHours()
+                            && match.getDate().getMinutes() == currentDate.getMinutes()
+                            && match.getDate().getSeconds() == currentDate.getSeconds()
+                    ) {
+                        Player ownerMatchPlayer = playerService.returnOwnerMatchPlayer(match);
+                        matchService.sendNotificationOfMatchEvaluation(match, ownerMatchPlayer);
+                        match.setMatchStatus(MatchStatus.PLAYED);
+                        matchService.save(match);
+                    }
                 }
+
             }
         } catch (Exception e) {
             log.info("no match has selected");
+        }
+    }
+    @Scheduled(cron = "* * * * * *")
+    public void changeMatchStatusTopending(){
+        Date currentDate  = new Date();
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(currentDate);
+        int currentYear = cal1.get(Calendar.YEAR);
+        int currentMonth = cal1.get(Calendar.MONTH);
+        int currentDay = cal1.get(Calendar.DAY_OF_MONTH);
+        try {
+            List<Match> listMatches = matchRepository.findAll();
+            for (Match match : listMatches) {
+                Calendar cal2 = Calendar.getInstance();
+                cal2.setTime(match.getDate());
+                int otherYear = cal2.get(Calendar.YEAR);
+                int otherMonth = cal2.get(Calendar.MONTH);
+                int otherDay = cal2.get(Calendar.DAY_OF_MONTH);
+                //add duration to start date of the match
+                cal2.add(Calendar.MINUTE,match.getDuration());
+                Date DatePlusDuration=cal2.getTime();
+                if (currentYear == otherYear && currentMonth == otherMonth && currentDay == otherDay)
+                {
+                   if (currentDate.after(match.getDate()) && currentDate.before(DatePlusDuration)){
+                       match.setMatchStatus(MatchStatus.PENDING);
+                   }
+                }
+            }
+        } catch (Exception e) {
+            log.info("no match is playing now");
         }
     }
 
